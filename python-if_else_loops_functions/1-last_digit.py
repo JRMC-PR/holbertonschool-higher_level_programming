7b0122480d9b5eecb211705f54c431be19faf164
JRMC-PR <towartohio@gmail.com>
2023-09-25 14:06:29 -0400

    exercice 1

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print("Last digit of", number, "is", end=" ")
+if number > 5:
+    print("and is grater that 5")
+elif number == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
