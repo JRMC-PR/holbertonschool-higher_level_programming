d789a34b3b8baa756850630fd87476287b736333
JRMC-PR <towartohio@gmail.com>
2023-09-26 13:56:15 -0400

    error fix 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 99:
-        # {:02d} means print 2 digits with leading 0
         print("{:02d}, ".format(i), end="")
     else:
         print("{:d}".format(i))
