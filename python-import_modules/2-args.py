c3513840ff64b6655f1d864325bdb4f1c4916a76
JRMC-PR <towartohio@gmail.com>
2023-09-27 14:45:37 -0400

    exercise 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    # * asigne values to argv and argc
+    argv = sys.argv
+    argc = len(sys.argv)
+    # * print the number of arguments
+    print("{} arguments:".format(argc - 1))
+    # * print the arguments
+    for i in range(1, argc):
+        print("{}: {}".format(i, argv[i]))
