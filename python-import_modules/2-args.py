9f3283d34f98d897d2ee0802986d9e667933f929
JRMC-PR <towartohio@gmail.com>
2023-09-27 15:17:46 -0400

    error fix 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if __name__ == "__main__":
     import sys
 # * asigne values to argv and argc
-argv = sys.argv
+# argv = sys.argv
 argc = len(sys.argv)
 # * print the number of arguments
 print("{} arguments.".format(argc - 1) if argc <=
-      1 else "{} argument:".format(argc - 1))
+      1 else "{} argumentss:".format(argc - 1))
 # * print the arguments
 for i in range(1, argc):
-    print("{}: {}".format(i, argv[i]))
+    print("{}: {}".format(i, sys.argv[i]))
