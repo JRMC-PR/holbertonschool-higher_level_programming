a0b34217f4b73d6248ece308efe2bb351fc339fd
JRMC-PR <towartohio@gmail.com>
2023-09-27 19:11:58 -0400

    exercise 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,11 +2,13 @@
 if __name__ == "__main__":
     import sys
 # * asigne values to argv and argc
-# argv = sys.argv
-argc = len(sys.argv)
-# * print the number of arguments
-print("{} arguments.".format(argc - 1) if argc <=
-      1 else "{} arguments:".format(argc - 1))
+argc = len(sys.argv)  # ? retrive the number of elements in argv
+if argc == 1:
+    print("0 arguments.")
+else:
+    # ? check argc and print print the number of arguments
+    print("{} argument{}:".format(argc - 1, "s" if argc > 2 else ""))
 # * print the arguments
 for i in range(1, argc):
     print("{}: {}".format(i, sys.argv[i]))
+rint("{} argument{}:".format(argc - 1, "s" if argc > 2 else ""))
