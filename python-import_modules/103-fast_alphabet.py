dae1e7234248a1d89f3c3c9abb9456d157557b9d
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-09-28 09:57:03 -0400

    error fixng

--- python-import_modules/103-fast_alphabet.py
+++ python-import_modules/103-fast_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-# ? import string module
-import string
-# ? print uppercase alphabet using string module
+import string  # ? import string module and use ascii_uppercase
 print(*string.ascii_uppercase, sep="")
