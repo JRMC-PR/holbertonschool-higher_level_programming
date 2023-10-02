8c0f7cd6719825fa1813a760b03a170b55dc421a
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-02 13:08:06 -0400

    Exercise 0

---
+++ python-data_structures/print_list_integer.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def priny_list_integer(my_list=[]):  # function that prints all integers of a list
+    for i in len(my_list):  # loop through list
+        print("{:d}".format(my_list[i]))  # print each element
