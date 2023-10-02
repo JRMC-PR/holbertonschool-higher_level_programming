037b0491f2d0ef06ab5bf9048946660b44b5493a
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-02 19:00:38 -0400

    exercise 3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-def print_reversed_list_
+def print_reversed_list_integer(my_list=[]):
+    if my_list:  # if my_list is not empty
+        for i in reversed(my_list):  # for each element in my_list, in reverse order
+            print("{:d}".format(i))
