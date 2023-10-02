86ab67f05711e77aef45c7c061128f6eb8be8fab
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-02 13:53:51 -0400

    exercise 1 python data and list s

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+def print_list_integer(my_list=[]):  # defines prints list function
+    for i in range(len(my_list)):  # for loop to iterate through list
+        print("{:d}".format(my_list[i]))  # prints list with format specifier
