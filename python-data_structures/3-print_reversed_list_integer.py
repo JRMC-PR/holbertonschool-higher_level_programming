7a07e9e75419e128aaf8e1990ce9bf270bcc4827
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-02 19:01:58 -0400

    pycode fix

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list:  # if my_list is not empty
-        for i in reversed(my_list):  # for each element in my_list, in reverse order
+        for i in reversed(my_list):  # go  in reverse order
             print("{:d}".format(i))
