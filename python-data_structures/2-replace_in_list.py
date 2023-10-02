e7e3367e12c0ab427db5b594c91ebb8620a013ac
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-02 18:50:28 -0400

    update exercise 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,4 +3,4 @@ def replace_in_list(my_list, idx, element):  # define method
     if idx < 0 or idx > len(my_list) - 1:  # move along the list
         return my_list
     else:
-        my_list[idx] = element
+        my_list.insert(idx, element)  # insert element at idx
