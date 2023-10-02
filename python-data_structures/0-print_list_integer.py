4a3c0565699595b7b30b9958f8d4a11dcfbefd9f
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-02 13:31:55 -0400

    error fixng

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def priny_list_integer(my_list=[]):  # function that prints all integers of a list
-    for i in range(len(my_list) - 1):  # loop through list
+    for i in range(len(my_list)):  # loop through list
         print("{:d}".format(my_list[i]))  # print each element
