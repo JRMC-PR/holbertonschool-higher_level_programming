6e62a7c660cc1f6c84d939e89ad62a227b716481
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-02 18:36:45 -0400

    exercise number 2

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):  # define method
+    if idx < 0 or idx > len(my_list) - 1:  # move along the list
+        return my_list
+    else:
+        my_list[idx] = element
