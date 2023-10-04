b012b6a80d077cf304c15dc24733922edc3eb4cb
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-03 20:48:33 -0400

    error fixing  11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if my_list is None:
         return None
-    if idx < 0 or idx > len(my_list):  # if idx is negative or out of range
+    if idx < 0 or idx > len(my_list) - 1:  # if idx is negative or out of range
         return my_list
     else:
         del my_list[idx]  # delete the element at idx
