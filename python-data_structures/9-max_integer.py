327f10bede16ad3649b040a715b4470a12c58640
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-04 09:47:16 -0400

    error fixing 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,6 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return None
-    else:
-        new_list = my_list.copy()  # copy the list
-        new_list.sort(reverse=True)  # sort the list
-        return new_list[0]
+    new_list = my_list.copy()  # copy the list
+    new_list.sort(reverse=True)  # sort the list
+    return new_list[0]
