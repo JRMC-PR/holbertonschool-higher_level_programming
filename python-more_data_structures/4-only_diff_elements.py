5a8f692019942493cc74ff3bedab76a0a84806a5
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-05 14:42:23 -0400

    error fixin g4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,3 @@
-#!/usr/bin/python3
+#!/usr/binpython3
 def only_diff_elements(set_1, set_2):
-   """ if set_1 is None:
-        return set_2
-    elif set_2 is None:
-        return set_1
     return set_1.union(set_2)
-"""
-    return = set_1.symmetric_difference(set_2)
