d28b9ae3b417b650651e812810c179377ebb1de9
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-04 20:43:57 -0400

    pycodestyle fix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,5 @@ def square_matrix_simple(matrix=[]):
     if matrix is None:
         return None
     m_copy = matrix.copy()
-    return list(map(lambda row: list(map(lambda colm: colm ** 2, row)), m_copy))
+    return list(map(lambda row:
+                    list(map(lambda colm: colm ** 2, row)), m_copy))
