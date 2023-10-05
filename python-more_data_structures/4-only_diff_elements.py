1ba2f31ff26f739f4bfde1f1c652a6b9c79ce555
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-05 14:35:10 -0400

    made it one line with symmetric_difference

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    if set_1 is None:
+   """ if set_1 is None:
         return set_2
     elif set_2 is None:
         return set_1
     return set_1.union(set_2)
+"""
+    return = set_1.symmetric_difference(set_2)
