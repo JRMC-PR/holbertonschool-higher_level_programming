0e01509c52482e1bf7bf02e2fff3eb125eecead7
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-05 18:42:04 -0400

    coments 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    # next line is a list comprehension
     for key, data in sorted(a_dictionary.items()):
         print(f"{key}: {data}")
