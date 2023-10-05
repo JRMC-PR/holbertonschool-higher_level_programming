7827f6d6bfef7d99bb45d45dde5cdffbb58cc032
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-04 21:02:48 -0400

    erro fix 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     if my_list is None:
         return None
     list_c = my_list.copy()
-    for i in range(len(list_c) - 1):
+    for i in range(len(list_c)):
         if list_c[i] == search:
             list_c[i] = replace
             return list_c
