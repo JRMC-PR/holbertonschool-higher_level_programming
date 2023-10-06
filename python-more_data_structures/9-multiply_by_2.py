ad5196ff0641d0f6030e3ef3f3405da31093a542
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-05 20:29:23 -0400

    exercise 9 Write a function that returns a new dictionary with all values multiplied by 2

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bon/python3
+def multiply_by_2(a_dictionary):
+    if a_dictionary is not None:
+        b_dictionary = a_dictionary.copy()
+        b_dictionary = {key: value * 2 for key, value in a_dictionary.items()}
+    return b_dictionary
