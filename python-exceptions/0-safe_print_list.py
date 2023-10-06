89bb2f0bf0454937bf786e385cc3385372fd2c1b
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-06 14:17:46 -0400

    errro fix 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,8 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(0, x):
-            print(f"{my_list[i]}")
+            print("{}".format(my_list[i]), end="")
+        print()
         return i + 1
-    except (IndexError, TypeError, ValueError):
+    except IndexError:
         print()
         return i
