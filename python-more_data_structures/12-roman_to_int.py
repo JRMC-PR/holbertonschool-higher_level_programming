9f8ad6be8390bc45bb45f3133b8ffba19ecfb610
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-06 12:15:05 -0400

    erro fix 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,7 @@ def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) != str:
         return 0
     else:
-        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                 'C': 100, 'D': 500, 'M': 1000}
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         num = 0
         print(len(roman_string))
         for i in range(len(roman_string)):
