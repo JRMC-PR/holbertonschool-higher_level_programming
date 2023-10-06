5ca555d6472b54053f747dc53d563e5b5f92e5b4
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-06 14:22:17 -0400

    errror fix 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if not my_list and x == 0:
+        return 0
     try:
         for i in range(0, x):
             print(f"{my_list[i]}".format(my_list[i]), end="")
