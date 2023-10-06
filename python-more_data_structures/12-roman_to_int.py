33cf284c49ca23b59391801d7adde4522eca8437
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-06 12:14:11 -0400

    erro fix 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,9 @@ def roman_to_int(roman_string):
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                  'C': 100, 'D': 500, 'M': 1000}
         num = 0
+        print(len(roman_string))
         for i in range(len(roman_string)):
+            print(f"{i} thisi in loop ")
             if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
                 num += roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
             else:
