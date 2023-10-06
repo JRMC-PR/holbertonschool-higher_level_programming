cfe4b52d3e1463e9e49e8569f4abe5766a34c9c8
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-05 20:06:39 -0400

    pycodestyle fix

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-    Update the dictionary `a_dictionary` with the key-value pair `(key, value)`.
-    If `key` is already in the dictionary, update its value to `value`.
-    If `key` is not in the dictionary, add the key-value pair `(key, value)` to the dictionary.
-    Return the updated dictionary.
+Update the dictionary `a_dictionary` with the key-value pair`(key, value)`.
+If `key` is already in the dictionary, update its value to `value`.
+If `key` is not in the dictionary, add the key-value pair
+`(key, value)` to the dictionary.
+Return the updated dictionary.
     """
 
 
