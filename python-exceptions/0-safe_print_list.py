6cde1140b3a074890efc7f292b142b95f2f51cc1
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-06 14:10:29 -0400

    errro fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        if my_list is None:
-            return 0
+
         for i in range(0, x):
             print(f"{my_list[i]}", end="")
-        print()
-        return i + 1
+        return True
     except (IndexError, TypeError, ValueError):
-        print()
-        return i
+        return False
