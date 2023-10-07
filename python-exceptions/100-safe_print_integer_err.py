63a77610358718efcab85309f43b5cfb5666ca7c
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-07 12:35:00 -0400

    exercise 8 Write a function that executes a function safely.

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,6 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except (ValueError, Exception):
+        # the next line prints the error to stderr
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
