e1ccba042a4853199b08056fa800e748b17036ed
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-11 09:24:10 -0400

    adding documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 class Square:
+    """
+    This is a Square class.
+
+    This class doesn't have any attributes or methods yet, but it can be used
+    as a starting point for creating a Square object.
+    """
     pass
