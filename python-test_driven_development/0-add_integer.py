e8abc69f2dbd7eff46770cbb22733fa1770982a7
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-12 12:50:26 -0400

    exercise 0 Write a function that adds 2 integers.

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a Function that adds 2 integers."""
+
+
+def add_integer(a, b=98):
+    """Return the sum of a and b
+    Args:
+        a (int): First integer
+        b (int): Second integer
+
+        >>> add_integer(1, 2)
+        3
+        >>> add_integer(100, -2)
+        98
+    """
+    """a Function that adds 2 integers."""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
