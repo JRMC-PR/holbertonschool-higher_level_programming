2572caa63767f5c807730ae10d5f327e40efaa04
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-13 19:35:52 -0400

    error fiz 4

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -20,7 +20,6 @@ def text_indentation(text):
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    text = text.strip()
     for i in range(len(text)):
         if text[i] == '.' or text[i] == '?' or text[i] == ':':
             print(text[i])
@@ -29,4 +28,4 @@ def text_indentation(text):
                 if text[i + 1] == ' ':
                     i += 1
         else:
-            print(text[i], end="")
+            print(text[i].strip(), end="")
