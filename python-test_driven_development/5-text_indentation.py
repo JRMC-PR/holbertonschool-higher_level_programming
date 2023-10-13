7381c32a2264688a1419f6a961b2de2e4b74d636
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-13 19:53:09 -0400

    error fix

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -22,6 +22,7 @@ def text_indentation(text):
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
+    text = text.strip()
     for i in range(len(text)):
         if text[i] == '.' or text[i] == '?' or text[i] == ':':
             print(text[i])
