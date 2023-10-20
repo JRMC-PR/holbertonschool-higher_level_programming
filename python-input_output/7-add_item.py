2553809a0d6812a66d65b5c55f598fb3a0e30e25
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-20 14:47:17 -0400

    commit updates exercise 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,11 +5,10 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-with open('add_item.json', 'a+') as f:
-    if f.read() != '':
-        lst = load_from_json_file('add_item.json')
-    else:
-        lst = []
-        for i in sys.argv[1:]:
-            lst.append(i)
-        save_to_json_file(lst, 'add_item.json')
+try:
+    lst = load_from_json_file('add_item.json')
+except:
+    lst = []
+    for i in sys.argv[1:]:
+        lst.append(i)
+    save_to_json_file(lst, 'add_item.json')
