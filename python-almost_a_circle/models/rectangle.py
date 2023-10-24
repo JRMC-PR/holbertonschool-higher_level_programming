85b9c67626540246756e7d1195652fa4f64bdfca
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-24 14:39:12 -0400

    commit does the same as the prev commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,10 +5,6 @@ from models.base import Base
 
 class Rectangle(Base):
     """ Rectangle class inherits from Base class """
-    __width = 0
-    __height = 0
-    __x = 0
-    __y = 0
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ init method for rectangle class
