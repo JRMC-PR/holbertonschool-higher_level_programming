152b77e860558a9801f1de02045a73604df1b6fa
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-24 11:28:31 -0400

    this commit reverts the chamges

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -40,8 +40,7 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
-    # Argument getters
-
+    """artgument getters"""
     @property
     def width(self):
         """ width getter """
