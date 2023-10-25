ce6a9a8c6f1c3ee5f9d15bb9437a54475be80720
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-25 11:24:02 -0400

    comit adds task 16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,6 +17,21 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ class method to write json string to file
+        Args:
+            list_objs (list): list of objects
+        """
+        import json
+        filename = cls.__name__ + ".json"
+        list_dict = []
+        if list_objs is not None:
+            for obj in list_objs:
+                list_dict.append(obj.to_dictionary())
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(list_dict))
+
     """static methods"""
 
     @staticmethod
