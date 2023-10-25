556270357e441383e238f6074084e68f97898da6
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-25 10:14:19 -0400

    commit updates task 12 (square)

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -43,7 +43,14 @@ class Square(Rectangle):
             kwargs (dict): dictionary of arguments
         """
         if args:
-            self.id, self.size, self.x, self.y = args
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
