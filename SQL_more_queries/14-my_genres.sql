7a563a6708c283e5ff05570cb555a1068d37186e
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-11-21 20:22:43 -0400

    Updates task 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,11 @@
 -- Script SELECT tv_genres.name AS genre,
-COUNT(tv_show_genres.genre_id) AS number_of_shows
+SELECT
+    tv_genres.name
 FROM
-    tv_show_genres
-    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY
-    genre
+    tv_genres
+    JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE
+    tv_shows.title = "Dexter"
 ORDER BY
-    number_of_shows DESC;
+    tv_genres.name ASC;
