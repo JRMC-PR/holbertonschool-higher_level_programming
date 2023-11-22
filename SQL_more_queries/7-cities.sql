97266274cbb2d43bf100eb5ccb2a4ef6c30f4495
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-11-21 20:06:36 -0400

    Updates task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,8 +3,9 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Create table
 CREATE TABLE IF NOT EXISTS cities (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT UNIQUE NOT NULL,
     state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL
-    FOREIGN KEY (state_id) REFERENCES states (id)
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
