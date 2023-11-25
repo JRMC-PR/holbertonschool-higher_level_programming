ad6c5adcd7a4fea11171f879c30f251bb1d4feb8
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-11-25 10:06:16 -0400

    Update task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
 
     """acreate the query and get the state
     name from the command line"""
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(
+    query = "SELECT * FROM states WHERE name LIKE BINARY \
+        '{}' ORDER BY states.id ASC".format(
         sys.argv[4])
 
     """Execute the query"""
