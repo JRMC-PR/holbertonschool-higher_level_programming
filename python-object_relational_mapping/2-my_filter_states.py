2a412440dcf63a6b9c16c0b69a2610f5bccbbc86
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-11-25 09:32:11 -0400

    Adds task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" a script that takes in an argument
+and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument."""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """Connect to the dabatabase"""
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3])
+
+    """Create a cursor object"""
+    cur = db.cursor()
+
+    """get the state name """
+    state_name = sys.argv[4]
+
+    """Execute a sql query"""
+    cur.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC", (state_name,))
+
+    """Fetch all rows"""
+    for row in cur.fetchall():
+        print(row)
+
+"""Close cursor and db"""
+cur.close()
+db.close()
